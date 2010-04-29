67649f49eb0ea4ac7bcaabfd339a4d21304c5058
Yigit Meltem <Yigit Meltem@localhost>
2010-04-29 20:37:12 +0100

    this thing is confusing lol

---
+++ GameEngine/Levels/LevelEntityCollection.cs
@@ -0,0 +1,33 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="LevelEntityCollection.cs" company="UAD">
+//   Game Design and Development
+// </copyright>
+// <summary>
+//   The level entity collection.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Gdd.Game.Engine.Levels
+{
+    using System.Collections.ObjectModel;
+
+    /// <summary>
+    /// The level entity collection.
+    /// </summary>
+    public class LevelEntityCollection : Collection<LevelEntity>
+    {
+        #region Properties
+
+        /// <summary>
+        /// Gets or sets Author.
+        /// </summary>
+        public string Author { get; set; }
+
+        /// <summary>
+        /// Gets or sets Script.
+        /// </summary>
+        public string[] Script { get; set; }
+
+        #endregion
+    }
+}
