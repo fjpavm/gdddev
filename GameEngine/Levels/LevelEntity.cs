f1182c39e6ddba7d793afc1227fd7709bfda0b14
Yigit Meltem <Yigit Meltem@localhost>
2010-04-29 13:34:42 +0100

    some new types

---
+++ GameEngine/Levels/LevelEntity.cs
@@ -0,0 +1,18 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="LevelEntity.cs" company="UAD">
+//   Game Design and Development
+// </copyright>
+// <summary>
+//   The level entity.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Gdd.Game.Engine.Levels
+{
+    /// <summary>
+    /// The level entity.
+    /// </summary>
+    public abstract class LevelEntity
+    {
+    }
+}
