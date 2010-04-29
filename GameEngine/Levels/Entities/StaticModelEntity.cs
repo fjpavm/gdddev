998cb864ee8835457ea6cfa4db77aeb6c95f70fd
Yigit Meltem <Yigit Meltem@localhost>
2010-04-29 13:34:42 +0100

    some new types

---
+++ GameEngine/Levels/Entities/StaticModelEntity.cs
@@ -0,0 +1,37 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="StaticModelEntity.cs" company="UAD">
+//   Game Design and Development
+// </copyright>
+// <summary>
+//   The static model entity.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Gdd.Game.Engine.Levels.Entities
+{
+    /// <summary>
+    /// The static model entity.
+    /// </summary>
+    [LevelEntityBinding("Gdd.Game.Engine.Levels.StaticModel, Gdd.Game.Engine")]
+    internal class StaticModelEntity : DrawableSceneComponentEntity
+    {
+        #region Properties
+
+        /// <summary>
+        /// Gets or sets GeometryType.
+        /// </summary>
+        public GeometryType GeometryType { get; set; }
+
+        /// <summary>
+        /// Gets or sets ModelDirection.
+        /// </summary>
+        public ModelDirection ModelDirection { get; set; }
+
+        /// <summary>
+        /// Gets or sets ModelName.
+        /// </summary>
+        public string ModelName { get; set; }
+
+        #endregion
+    }
+}
