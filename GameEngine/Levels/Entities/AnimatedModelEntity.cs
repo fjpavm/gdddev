b1f615a1545418b6bf1fb73a7c6da8461bd06d23
Yigit Meltem <Yigit Meltem@localhost>
2010-05-01 15:54:06 +0100

    added new entity classes. cleaned artifacts from the old serialization methods

---
+++ GameEngine/Levels/Entities/AnimatedModelEntity.cs
@@ -0,0 +1,20 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AnimatedModelEntity.cs" company="UAD">
+//   Game Design and Development
+// </copyright>
+// <summary>
+//   The animated model entity.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Gdd.Game.Engine.Levels.Entities
+{
+    /// <summary>
+    /// The animated model entity.
+    /// </summary>
+    [LevelEntityBinding("Gdd.Game.Engine.Levels.AnimatedModel, Gdd.Game.Engine")]
+    [LevelEntityCategory("Characters")]
+    public class AnimatedModelEntity : StaticModelEntity
+    {
+    }
+}
