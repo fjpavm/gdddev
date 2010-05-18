bf5e75b1e72ce69499c28246f5f210f872b41a67
Tony <Tony@localhost>
2010-05-18 16:12:37 +0100

    Initialize new sound files and test them in main menu

--- GameEngine/Audio.cs
+++ GameEngine/Audio.cs
@@ -37,6 +37,67 @@ namespace Gdd.Game.Engine
         /// Menu click music cue, used for several methods here.
         /// </summary>
         static Cue menuClick;
+
+        /// <summary>
+        /// End dialogue cue, used for several methods here.
+        /// </summary>
+        static Cue endDialogue;
+
+        /// <summary>
+        /// Introduction diialogue cue, used for several methods here.
+        /// </summary>
+        static Cue introductionDialogue;
+        
+
+        /// <summary>
+        /// Hero dying music cue, used for several methods here.
+        /// </summary>
+        static Cue heroDying1;
+
+        /// <summary>
+        /// Hero dying music cue, used for several methods here.
+        /// </summary>
+        static Cue heroDying2;
+
+        /// <summary>
+        /// Attacking flower music cue, used for several methods here.
+        /// </summary>
+        static Cue attackingFlower1;
+
+        /// <summary>
+        /// Attacking flower music cue, used for several methods here.
+        /// </summary>
+        static Cue attackingFlower2;
+
+        /// <summary>
+        /// Attacking flower music cue, used for several methods here.
+        /// </summary>
+        static Cue attackingFlower3;
+
+        /// <summary>
+        /// Attacking flower music cue, used for several methods here.
+        /// </summary>
+        static Cue attackingFlower4;
+
+        /// <summary>
+        /// Dying flower music cue, used for several methods here.
+        /// </summary>
+        static Cue dyingFlower1;
+
+        /// <summary>
+        /// Dying flower music cue, used for several methods here.
+        /// </summary>
+        static Cue dyingFlower2;
+
+        /// <summary>
+        /// Dying flower music cue, used for several methods here.
+        /// </summary>
+        static Cue dyingFlower3;
+
+        /// <summary>
+        /// Dying flower music cue, used for several methods here.
+        /// </summary>
+        static Cue dyingFlower4;
         #endregion
 
         #region Constructor
@@ -47,12 +108,30 @@ namespace Gdd.Game.Engine
         {
             // Loading audio
             audioEngine = new AudioEngine("Content\\Audio\\DreamerAudio.xgs");
-            waveBank = new WaveBank(audioEngine, "Content\\Audio\\Wave Bank.xwb");
-            soundBank = new SoundBank(audioEngine, "Content\\Audio\\Sound Bank.xsb");
+            waveBank    = new WaveBank(audioEngine, "Content\\Audio\\Wave Bank.xwb");
+            soundBank   = new SoundBank(audioEngine, "Content\\Audio\\Sound Bank.xsb");
 
-            // Get background music cue
+            // Get music cues
             backgroundMusic = soundBank.GetCue("GameBackgroundMusic");
             menuClick = soundBank.GetCue("MenuClick");
+
+            endDialogue = soundBank.GetCue("EndDialogue");
+            introductionDialogue = soundBank.GetCue("IntroductionDialogue");
+
+            heroDying1 = soundBank.GetCue("FranciscoHeroDying1");
+            heroDying2 = soundBank.GetCue("FranciscoHeroDying2");
+
+            attackingFlower1 = soundBank.GetCue("LukeAttackingFlower");
+            attackingFlower2 = soundBank.GetCue("SarahAttackingFlower");
+            attackingFlower3 = soundBank.GetCue("ShringiFlowerAttacking");
+            attackingFlower4 = soundBank.GetCue("VolkanFlowerAttacking");
+
+            dyingFlower1 = soundBank.GetCue("LukeDyingFlower");
+            dyingFlower2 = soundBank.GetCue("SarahDyingFlower");
+            dyingFlower3 = soundBank.GetCue("ShringiFlowerDying");
+            dyingFlower4 = soundBank.GetCue("VolkanFlowerDying");
+
+
 
         }
         #endregion
@@ -82,6 +161,130 @@ namespace Gdd.Game.Engine
             }
             backgroundMusic.Play();
         }
+
+        /// <summary>
+        /// Play end dialogue sound
+        /// </summary>
+        public static void PlayEndDialogue()
+        {
+            if (!endDialogue.IsPrepared)
+            {
+                endDialogue = soundBank.GetCue("EndDialogue");
+            }
+            endDialogue.Play();
+        }
+
+        /// <summary>
+        /// Play introduction dialogue sound
+        /// </summary>
+        public static void PlayIntroductionDialogue()
+        {
+            if (!introductionDialogue.IsPrepared)
+            {
+                introductionDialogue = soundBank.GetCue("IntroductionDialogue");
+            }
+            introductionDialogue.Play();
+        }
+
+        /// <summary>
+        /// Play hero dying sound
+        /// </summary>
+        public static void PlayHeroDying1()
+        {
+            if (!heroDying1.IsPrepared)
+            {
+                heroDying1 = soundBank.GetCue("FranciscoHeroDying1");
+            }
+            heroDying1.Play();
+        }
+
+        public static void PlayHeroDying2()
+        {
+            if (!heroDying2.IsPrepared)
+            {
+                heroDying2 = soundBank.GetCue("FranciscoHeroDying2");
+            }
+            heroDying2.Play();
+        }
+
+        /// <summary>
+        /// Play flower attacking sound
+        /// </summary>
+        public static void PlayAttackingFlower1()
+        {
+            if (!attackingFlower1.IsPrepared)
+            {
+                attackingFlower1 = soundBank.GetCue("LukeAttackingFlower");
+            }
+            attackingFlower1.Play();
+        }
+
+        public static void PlayAttackingFlower2()
+        {
+            if (!attackingFlower2.IsPrepared)
+            {
+                attackingFlower2 = soundBank.GetCue("SarahAttackingFlower");
+            }
+            attackingFlower2.Play();
+        }
+
+        public static void PlayAttackingFlower3()
+        {
+            if (!attackingFlower3.IsPrepared)
+            {
+                attackingFlower3 = soundBank.GetCue("ShringiFlowerAttacking");
+            }
+            attackingFlower3.Play();
+        }
+
+        public static void PlayAttackingFlower4()
+        {
+            if (!attackingFlower4.IsPrepared)
+            {
+                attackingFlower4 = soundBank.GetCue("VolkanFlowerAttacking");
+            }
+            attackingFlower4.Play();
+        }
+
+        /// <summary>
+        /// Play flower dying sound
+        /// </summary>
+        public static void PlayDyingFlower1()
+        {
+            if (!dyingFlower1.IsPrepared)
+            {
+                dyingFlower1 = soundBank.GetCue("LukeDyingFlower");
+            }
+            dyingFlower1.Play();
+        }
+
+        public static void PlayDyingFlower2()
+        {
+            if (!dyingFlower2.IsPrepared)
+            {
+                dyingFlower2 = soundBank.GetCue("SarahDyingFlower");
+            }
+            dyingFlower2.Play();
+        }
+
+        public static void PlayDyingFlower3()
+        {
+            if (!dyingFlower3.IsPrepared)
+            {
+                dyingFlower3 = soundBank.GetCue("ShringiFlowerDying");
+            }
+            dyingFlower3.Play();
+        }
+
+        public static void PlayDyingFlower4()
+        {
+            if (!dyingFlower4.IsPrepared)
+            {
+                dyingFlower4 = soundBank.GetCue("VolkanFlowerDying");
+            }
+            dyingFlower4.Play();
+        }
+
         #endregion
 
         #region Stop, Pause and Resume background music
